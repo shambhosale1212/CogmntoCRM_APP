714f9dfbc410a8443ef9772776e3bf641c796208
unknown <shambhosale1212@gmail.com>
2024-07-19 10:41:02 +0530

    Cogmento Framework is creared by Master

---
+++ src/main/java/BaseLayer/BaseClass.java
@@ -0,0 +1,48 @@
+package BaseLayer;
+
+import java.io.FileInputStream;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+	protected static Properties prop;
+	protected static FileInputStream fis;
+	protected static WebDriver driver;
+	
+	
+	
+	public BaseClass()
+	{
+		prop = new Properties();
+		
+		String path= System.getProperty("user.dir")+"/src/main/java/ConfigLayer/Config.proprties";
+		try {	
+		fis = new FileInputStream(path);
+		prop.load(fis);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+	public static void initialization()
+	{
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30)).pageLoadTimeout(Duration.ofSeconds(30));
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		
+		String url=prop.getProperty("URL");
+		driver.get(url);
+	}
+	
+	
+	
+	
+	
+	
+
+}
