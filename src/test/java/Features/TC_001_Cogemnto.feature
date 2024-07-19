8e4e72a26fe16b577c1f16e3f392bc6539a15651
Ashok <aashokgambhire999@gmail.com>
2024-07-19 11:45:40 +0530

    freature file is created by ashok

---
+++ src/test/java/Features/TC_001_Cogemnto.feature
@@ -0,0 +1,10 @@
+URL=https://ui.cogmento.com/
+
+USERNAME=prafulp1010@gmail.com
+
+PASSWORD=Pr@ful0812Feature: Test cogmento CRM Application
+
+  Scenario: Valiate login functionality
+    Given user is on login page
+    When user enter valid cradentials
+    Then user click on login button
---
+++ src/test/java/Steps/Loginpagetest.java
@@ -0,0 +1,48 @@
+package Steps;
+
+import BaseLayer.BaseClass;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import BaseLayer.BaseClass;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Loginpagetest extends BaseClass{
+	
+	@Given("user is on login page")
+	public void user_is_on_login_page() {
+	    
+	}
+
+	@When("user enter valid cradentials")
+	public void user_enter_valid_cradentials() {
+	   
+	}
+
+	@Then("user click on login button")
+	public void user_click_on_login_button() {
+	   
+	}
+
+
+
+}
